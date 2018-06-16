46ce8965a9a064276070ad68dc6789f24970139e
Alex Kharchenko <aleksei8696@gmail.com>
2018-06-16 21:31:42 +0300

    добавлен выход

--- app/src/main/java/com/example/aleks/fuckyoufuckingfuck/MainActivity.java
+++ app/src/main/java/com/example/aleks/fuckyoufuckingfuck/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.aleks.fuckyoufuckingfuck;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,8 @@ import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -93,6 +97,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_signout){
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    Snackbar.make(activity_main, "Выход выполнен", Snackbar.LENGTH_SHORT).show();
+                    finish();
+
+                }
+            });
+        }
         return true;
     }
 }
